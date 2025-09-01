787d92f84f8fc46fa6d1f13d53fea4ecfed78908
KamilSMKN2SKA <hisyamkamilofc@gmail.com>
2025-09-01 08:25:19 +0700

    Add files via upload

---
+++ latenuma2.py
@@ -0,0 +1,25 @@
+# a = int(input("Masukkan angka pertama :"))
+# b = int(input("Masukkan angka kedua :"))
+
+# for i in range (a,b,):
+#     if i % 2 == 0 :
+#         print(i, "adalah Genap")
+#     else:
+#         print(i, "adalah Ganjil")
+
+
+
+c = 1
+d = 0
+n = int(input("Masukkan angka : "))
+
+while c <= n:
+    d = d + c
+    c += 1
+
+print(d)
+
+# n = int(input("Masukkan angka yang ingin dikali : "))
+
+# for i in range (1,11):
+#     print(f"{n} x {i} = {(n)*(i)}" )
